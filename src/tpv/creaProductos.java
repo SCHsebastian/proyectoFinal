5087ebe6d20123e1102833c57b60e38f6ee08451
Sebastián <schsebastiancardonahenao@gmail.com>
2021-06-04 16:16:41 +0200

    update

---
+++ src/tpv/creaProductos.java
@@ -0,0 +1,22 @@
+package tpv;
+
+import java.io.IOException;
+/*
+public class creaProductos {
+	
+	public static void main(String[] args) throws IOException {
+		TodoProductos.anyadirUnProducto(new Producto("Memoria TeamGroup 16gb",90.10,"RAM","ImagenesProductos\\8x2 TEAM GROUP_opt.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("Memoria VENGANCE 2X8 16gb",80.99,"RAM","ImagenesProductos\\VENGANCE 16GB_opt.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("Cable PCIE",3,"Tarjeta Gráfica","ImagenesProductos\\cable.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("HDD 1TB Barracuda",53.50,"Disco Duro","ImagenesProductos\\HDD-1TB.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("GTX 1650",301.99,"Tarjeta Gráfica","ImagenesProductos\\gtx1650_opt.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("RTX4000",1200,"Tarjeta Gráfica","ImagenesProductos\\RTX4000_opt.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("Caja Gráfica",200,"Tarjeta Gráfica","ImagenesProductos\\cajaGrafica.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("GTX 1660",449.99,"Tarjeta Gráfica","ImagenesProductos\\gtx1660_opt.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("I3 10100",150,"Procesador","ImagenesProductos\\i3-10100_opt.jpg"));
+		TodoProductos.anyadirUnProducto(new Producto("Zotac GT710",80,"Tarjeta Gráfica","ImagenesProductos\\gt710_1gb.jpg"));		
+		System.out.println("todo creado");
+	}
+
+
+}*/
